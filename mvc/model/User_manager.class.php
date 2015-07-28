3d1adfb4c9b9e0b307473e78be87441e6f953be2
Öner <oner@MacBook-de-Greg.local>
2015-07-28 12:05:11 +0200

    add gestion-user

---
+++ mvc/model/User_manager.class.php
@@ -0,0 +1,53 @@
+<?php 
+Class User_manager{
+
+	private $users = array();
+	
+	function get_nb_user_online(){
+		// return array
+		$online_users = array();
+		foreach ($this->users as $row) {
+			if ($row->get_statut) $online_users[] = $row;			
+		}
+		return $online_users;
+	}
+
+	function search_user($nom="", $prenom=""){
+		//retourne tableau de resultat
+		$result = array();
+		foreach ($this->users as $row) {
+			if ($row->get_nom() = $nom ||
+					$row->get_prenom = $prenom ||
+					($row->get_nom = $nom && $row->get_prenom = $prenom)) {
+				$result[] = $row;
+			}
+		}
+		return $result;
+	}
+
+	function delete_user($user){
+		/*
+		params : mail
+		return : boolean
+		*/
+
+		foreach ($this->users as $key => $row) {
+			if($row->get_mail() == $user->get_mail()){
+				unset($this->users[$key]);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function add_user($user){
+		// return tableau users
+		$this->users[] = $user;
+		return $this->users;
+	}
+
+	
+
+}
+
+ ?>
