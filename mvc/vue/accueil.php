b88179d6901bc533f05662dbf10c9c340ee9d6b9
gilles88 <montmirel.gilles@gmail.com>
2015-07-28 13:42:28 +0200

    Dep index

---
+++ mvc/vue/accueil.php
@@ -0,0 +1,29 @@
+<nav>
+    <a href="#" data-target="">Utilisateur</a>
+    <a href="#" data-target="">Camions</a>
+    <a href="#" data-target="">Missions</a>
+    <span class="user_connect">Gilles Montmirel</span>
+    <span class="nb_connect">24</span>
+</nav>
+
+<div class="content">
+
+    <div class="head">
+        <div class="search">
+            <div class="search_form">
+                <form action="">
+                    <input type="text" class="form-control" placeholder="prénom" name="prenom">
+                    <input type="text" class="form-control" placeholder="nom" name="nom">
+                    <button class="btn btn-primary">Rechercher</button>
+                </form>
+            </div>
+            <a href="#">Add chauffeur</a>
+            <a href="#">Add gestionnaire</a>                    
+        </div>
+    </div>
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+    Amet ab aliquid, cupiditate veritatis dolorum earum! Natus, aut. 
+    Hic qui illum rem, iste mollitia, ea assumenda, necessitatibus rerum 
+    labore odit neque.
+</div>
